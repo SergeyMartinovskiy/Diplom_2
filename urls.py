b44cb6ac347f9ddf2cba5392ab39fe460346e141
Ser Martin <ser.martinovskiy@yandex.ru>
2025-04-29 10:31:33 +0300

    First Commit

--- urls.py
+++ urls.py
@@ -3,4 +3,7 @@
 class URLS:
     URL = 'https://stellarburgers.nomoreparties.site/'
     URL_CREATE_USERS = f'{URL}api/auth/register'
+    URL_LOGIN_USERS = f'{URL}api/auth/login'
+    URL_CREATE_ORDER = f'{URL}api/orders'
+
 
