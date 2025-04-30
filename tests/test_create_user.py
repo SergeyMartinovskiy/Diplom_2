4114fdfa164e68e51810152de0d46b9d4b03cd80
Ser Martin <ser.martinovskiy@yandex.ru>
2025-04-30 11:02:35 +0300

    First Commit

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -5,6 +5,7 @@ import pytest
 from base_api import BaseApi
 from data import gen_user_data
 
+# принты оставил для понимания выводов и отслеживания значений
 
 class TestCreateUser:
     @allure.title('Проверка создания уникального пользователя, возврат ответа код 200')
@@ -25,6 +26,12 @@ class TestCreateUser:
     def test_try_create_user_already_exist(self):
         data = gen_user_data()
         response = BaseApi.create_new_user(**data)
+
+        print(f"Request URL: {response.url}")
+        print(f"Request Body: {response.request.body}")
+        print(f"Response Status Code: {response.status_code}")
+        print(f"Response Body: {response.json()}")
+
         assert response.status_code == 200
         response_1 = BaseApi.create_new_user(**data)
 
@@ -34,6 +41,21 @@ class TestCreateUser:
         print(f"Response Body: {response_1.json()}")
 
         assert response_1.status_code == 403
+
+    @allure.title('Проверка получения ошибки 403 при создании пользователя с незаполненным одним из полей')
+    @pytest.mark.parametrize('empty_necessary_field', ["email", 'password', 'name'])
+    def test_try_create_user_with_empty_necessary_field(self, empty_necessary_field):
+        data = gen_user_data()
+        data[empty_necessary_field] = ''
+
+        response = BaseApi.create_new_user(**data)
+
+        print(f"Request URL: {response.url}")
+        print(f"Request Body: {response.request.body}")
+        print(f"Response Status Code: {response.status_code}")
+        print(f"Response Body: {response.json()}")
+
+        assert response.status_code == 403
 
 
 
