ac5a3995e189ae73e68d829c1c3f4607be9d7b2d
Ser Martin <ser.martinovskiy@yandex.ru>
2025-05-04 10:56:54 +0300

    First Commit

--- tests/test_change_data_users.py
+++ tests/test_change_data_users.py
@@ -3,11 +3,12 @@ import pytest
 import conftest
 from base_api import BaseApi
 
+
 from base_api import BaseApi
-from data import gen_user_data
+from data import gen_user_data, gen_fake_email
 
 class TestChangeDataUser:
-    @allure.title('Проверка изменения данных неавторизованного пользователя ')
+    @allure.title('Проверка невозможности изменения данных неавторизованного пользователя ')
     def test_change_data_unuthorised_user(self):
         data = gen_user_data()
         response = BaseApi.create_new_user(**data)
@@ -22,4 +23,36 @@ class TestChangeDataUser:
         response_update = BaseApi.change_date_user(token=None, **update_data)
         assert response_update.status_code == 401
         assert 'You should be authorised' in response_update.json().get('message')
+
+    @allure.title('Проверка успешного обновления данных пользователя')
+    @pytest.mark.parametrize('update_type',
+                             ['email', 'name', 'both'])
+    def test_succesfull_change_data_user(self, update_type):
+        data = gen_user_data()
+        response = BaseApi.create_new_user(**data)
+        assert response.status_code == 200
+
+        response_1 = BaseApi.login_user(email=data['email'], password=data['password'])
+        assert response_1.status_code == 200
+        assert 'accessToken' in response_1.json()
+
+        token = response_1.json()['accessToken']
+
+        update_data = {}
+        if update_type in ['email', 'both']:
+            update_data['email'] = gen_fake_email()
+        if update_type in ['name', 'both']:
+            update_data['name'] = 'NewSerg'
+
+        response_update = BaseApi.change_date_user(token, **update_data)
+
+        print('Данные для обновления:', update_data)
+
+        assert response_update.status_code == 200
+
+
+        delete_response = BaseApi.delete_user(token)
+        assert delete_response.status_code == 202
+
+
 
