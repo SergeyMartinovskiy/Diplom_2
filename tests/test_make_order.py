c029c1b71c730318010bb5d144807fc34ab15ade
Ser Martin <ser.martinovskiy@yandex.ru>
2025-05-05 09:33:56 +0300

    First Commit

--- tests/test_make_order.py
+++ tests/test_make_order.py
@@ -23,4 +23,34 @@ class TestMakeOrder:
         assert response_2.status_code == 200
         assert response_2.json()['success'] is True
 
+    @allure.title('Проверка невозможности создания заказа без авторизации')
+    @allure.description('Зафиксирован баг.'
+                        'ОР: неавторизованный пользователь не может создать заказ. Код ответа 401'
+                        'ФР: неавторизованный пользователь может создать заказ. Код ответа 200')
+    def test_make_order_without_authorisation(self):
+        ingredients_response = BaseApi.get_ingredients()
+        available_ingredients = ingredients_response.json()['data']
+        order_ingredients = generator_data_of_order(available_ingredients)
+
+        response_2 = BaseApi.make_order(token = '', ingredients = order_ingredients)
+        assert response_2.status_code == 400
+        assert response_2.json()['success'] is False
+
+    @allure.title('Проверка возможности создания заказа без ингридиентов')
+    def test_make_order_without_ingredients(self):
+        data = gen_user_data()
+        response = BaseApi.create_new_user(**data)
+        BaseApi.login_user(email=data['email'], password=data['password'])
+
+        token = response.json().get('accessToken')
+
+        response_2 = BaseApi.make_order(token=token, ingredients='')
+        assert response_2.status_code == 400
+        assert 'Ingredient ids must be provided' in response_2.json().get('message')
+
+
+
+
+
+
 
