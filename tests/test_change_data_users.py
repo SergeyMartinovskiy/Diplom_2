f37bfe153dbe243778ac44821f46e7e413c26fad
Ser Martin <ser.martinovskiy@yandex.ru>
2025-05-13 12:14:53 +0300

    First Commit

--- tests/test_change_data_users.py
+++ tests/test_change_data_users.py
@@ -19,52 +19,38 @@ class TestChangeDataUser:
         assert 'You should be authorised' in response_update.json().get('message')
 
     @allure.title('Проверка успешного обновления email пользователя')
-    def test_successful_change_email_user(self):
-        data = gen_user_data()
-        BaseApi.create_new_user(**data)
+    def test_successful_change_email_user(self, create_user):
+        data = create_user
         response_1 = BaseApi.login_user(email=data['email'], password=data['password'])
         token = response_1.json()['accessToken']
-
         update_data = {
             'email': gen_fake_email()
         }
-
         response_update = BaseApi.change_date_user(token, **update_data)
         assert response_update.status_code == 200
-        delete_response = BaseApi.delete_user(token)
-        assert delete_response.status_code == 202
+        assert response_update.json()['success'] is True
 
     @allure.title('Проверка успешного обновления имени пользователя')
-    def test_successful_change_name_user(self):
-        data = gen_user_data()
-        BaseApi.create_new_user(**data)
+    def test_successful_change_name_user(self, create_user):
+        data = create_user
         response_1 = BaseApi.login_user(email=data['email'], password=data['password'])
         token = response_1.json()['accessToken']
-
         update_data = {
             'name': 'NewSerg'
         }
-
         response_update = BaseApi.change_date_user(token, **update_data)
         assert response_update.status_code == 200
-        delete_response = BaseApi.delete_user(token)
-        assert delete_response.status_code == 202
+        assert response_update.json()['success'] is True
 
     @allure.title('Проверка успешного обновления email и имени пользователя')
-    def test_successful_change_email_and_name_user(self):
-        data = gen_user_data()
-        BaseApi.create_new_user(**data)
+    def test_successful_change_email_and_name_user(self, create_user):
+        data = create_user
         response_1 = BaseApi.login_user(email=data['email'], password=data['password'])
         token = response_1.json()['accessToken']
-
         update_data = {
             'email': gen_fake_email(),
             'name': 'NewSerg'
         }
-
         response_update = BaseApi.change_date_user(token, **update_data)
         assert response_update.status_code == 200
-        delete_response = BaseApi.delete_user(token)
-        assert delete_response.status_code == 202
-
-
+        assert response_update.json()['success'] is True
