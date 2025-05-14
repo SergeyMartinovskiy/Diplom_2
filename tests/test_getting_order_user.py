6ecb002e8b092066f69bc8cf7fb4bac74c1c41be
Ser Martin <ser.martinovskiy@yandex.ru>
2025-05-14 22:07:14 +0300

    First Commit

--- tests/test_getting_order_user.py
+++ tests/test_getting_order_user.py
@@ -6,17 +6,16 @@ from data import gen_user_data
 class TestGettingOrderUser:
 
     @allure.title('Получение заказов авторизованного пользователя')
-    def test_getting_order_authorised_user(self):
-        data = gen_user_data()
-        response = BaseApi.create_new_user(**data)
-        BaseApi.login_user(email=data['email'], password=data['password'])
+    def test_getting_order_authorised_user(self, create_user):
+        data = create_user
+
+        response = BaseApi.login_user(email=data['email'], password=data['password'])
         token = response.json().get('accessToken')
         response_2 = BaseApi.getting_orders_user(token)
         assert response_2.status_code == 200
         assert response_2.json()['success'] is True
 
-        delete_response = BaseApi.delete_user(token)
-        assert delete_response.status_code == 202
+
 
     @allure.title('Получение заказов неавторизованного пользователя')
     def test_getting_order_unauthorised_user(self):
